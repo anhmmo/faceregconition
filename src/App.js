c3598e5ae7b501fc36225a1c6ed080301729263b
anhmmo <aun@metropolia.fi>
2020-03-27 17:48:37 +0200

    import components files to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import FaceRecognition from './Components/FaceRegconition/FaceRegconition';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
     </div>
   );
 }
