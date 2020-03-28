bdfe8912bba3d6ec5456212f7b2df1e249d8d897
anhmmo <aun@metropolia.fi>
2020-03-28 20:21:55 +0200

    return styled logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
 
 function Logo() {
     return (
-        <div>
-            
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner"> 👽 </div>
+            </Tilt>
         </div>
     )
 }
 
-export default Logo
+export default Logo;
