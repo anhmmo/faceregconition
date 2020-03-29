410f1e6ee8fef9311d14534281d520fd0fd03ed0
anhmmo <aun@metropolia.fi>
2020-03-29 14:41:56 +0300

    add particles to App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import FaceRecognition from './Components/FaceRegconition/FaceRegconition';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+import Particles from 'react-particles-js';
+
 
 function App() {
+
   return (
     <div className="App">
+      <Particles 
+        className="praticles" 
+        params={{
+          particles: {
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: "#fff",
+                blur: 10
+              }
+            }
+          }
+        }
+        }  
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       <FaceRecognition />
     </div>
