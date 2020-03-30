a2c199e6c2bc8d7f175bf8f9e2f799fdfa15456a
anhmmo <aun@metropolia.fi>
2020-03-30 22:55:44 +0300

    updated oninputChange

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -7,6 +7,7 @@ const ImageLinkForm = ({onInputChange}) => {
             <p className="f3 white">
                 {'This Magic Brain will detect faces in your picture. Give it a try'}
             </p>
+            
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
                     <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange}/>
