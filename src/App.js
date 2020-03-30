a1cb7349d26bf9884a6eb579413d13794e25c19f
anhmmo <aun@metropolia.fi>
2020-03-30 22:45:28 +0300

    convert App.js to Class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import FaceRecognition from './Components/FaceRegconition/FaceRegconition';
@@ -8,32 +8,41 @@ import './App.css';
 import Particles from 'react-particles-js';
 
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
 
-  return (
-    <div className="App">
-      <Particles 
-        className="praticles" 
-        params={{
-          particles: {
-            line_linked: {
-              shadow: {
-                enable: true,
-                color: "#fff",
-                blur: 10
+render(){
+    return (
+      <div className="App">
+        <Particles 
+          className="praticles" 
+          params={{
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#fff",
+                  blur: 10
+                }
               }
             }
           }
-        }
-        }  
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      <FaceRecognition />
-    </div>
-  );
+          }  
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        <FaceRecognition />
+      </div>
+    );
+  }
+
 }
 
 export default App;
