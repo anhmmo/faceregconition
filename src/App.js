0364ae0c66ac9277ce65706855a1ece695a77ebd
anhmmo <aun@metropolia.fi>
2020-03-31 21:57:48 +0300

    install and inport Clarify.ai

--- src/App.js
+++ src/App.js
@@ -6,18 +6,38 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
+
+const app = new Clarifai.App({
+  apiKey: 'ca98921915544a3da2f8edafcc57a7a8'
+ });
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+    Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   }
 
 render(){
@@ -41,8 +61,8 @@ render(){
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
-        <FaceRecognition />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
