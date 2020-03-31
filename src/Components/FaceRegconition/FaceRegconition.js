afc81b39baf14ca24921440ec46b7bd9809045a1
anhmmo <aun@metropolia.fi>
2020-03-31 21:57:06 +0300

    style image box  and add image url from input

--- src/Components/FaceRegconition/FaceRegconition.js
+++ src/Components/FaceRegconition/FaceRegconition.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './FaceRegconition.css';
 
-function FaceRegconition() {
+function FaceRegconition({imageUrl}) {
     return (
-        <div>
-            
+        <div className="center ma">
+            <div className="absolute mt2">
+                <img src={imageUrl} alt="" />
+            </div>
         </div>
     )
 }
