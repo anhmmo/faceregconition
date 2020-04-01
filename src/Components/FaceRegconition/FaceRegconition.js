b2a46461611d3394f2fe7daaeb0b69c07c8a31e3
anhmmo <aun@metropolia.fi>
2020-04-01 20:33:04 +0300

    add imageUrl

--- src/Components/FaceRegconition/FaceRegconition.js
+++ src/Components/FaceRegconition/FaceRegconition.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './FaceRegconition.css';
 
-function FaceRegconition({imageUrl}) {
+function FaceRegconition({imageUrl, box}) {
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img src={imageUrl} alt="" />
+                <img id='inputImage' src={imageUrl} alt="" />
+                <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     )
